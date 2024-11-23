80c943ff543067e1d859a34277c2a9c1cc6dee8e
Alonzzoh <edalopp@hotmail.com>
2024-11-23 13:08:17 -0500

    lil fix to bar (LERP)

--- Assets/Scripts/Boss/ClickPlayer.cs
+++ Assets/Scripts/Boss/ClickPlayer.cs
@@ -7,11 +7,13 @@ using UnityEngine.UI;
 public class ClickPlayer : MonoBehaviour
 {
     [SerializeField] private Image clickBarFull;
+    [SerializeField] private float maxClicks;
      private float clickcount = 0;
 
 
     [SerializeField] private float timer;
     private float time = 0;
+
 
 
 
@@ -25,8 +27,9 @@ public class ClickPlayer : MonoBehaviour
             time = 0;
         }
 
+
         ClickCounter();
-        clickBarFull.fillAmount = clickcount / 100f;
+        clickBarFull.fillAmount = Mathf.Lerp(clickBarFull.fillAmount ,clickcount / maxClicks, Time.deltaTime * 20f);
     }
 
     void ClickCounter()
