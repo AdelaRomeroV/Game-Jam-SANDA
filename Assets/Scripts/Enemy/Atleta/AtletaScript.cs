160af3fa15401541ef485d4ec198b85ec1e2dea2
S_K12 <elchavez012@gmail.com>
2024-11-23 19:05:27 -0500

    Fix Lifebar / PlayerDamage

--- Assets/Scripts/Enemy/Atleta/AtletaScript.cs
+++ Assets/Scripts/Enemy/Atleta/AtletaScript.cs
@@ -4,16 +4,25 @@ using UnityEngine;
 
 public class AtletaScript : MonoBehaviour
 {
-    public float speed = 5f; 
+    public float speed = 5f;
+    private Rigidbody2D rb;
 
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        rb.velocity = Vector2.down * speed;
     }
 
     void Update()
-    {   
-        transform.Translate(Vector2.down * speed * Time.deltaTime);
+    {
+
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            Debug.Log("Atleta ha tocado al jugador");
+        }
     }
     private void OnBecameInvisible()
     {        
--- Assets/Scripts/Player/Movement/PlayerMovement.cs
+++ Assets/Scripts/Player/Movement/PlayerMovement.cs
@@ -1,19 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class PlayerMovement : MonoBehaviour
 {
     [Header("Componentes")]
     [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private SpriteRenderer spriteRenderer;
 
     [Header("Movimiento")]
     [SerializeField] private float movSpeed;
-    Vector2 velocity;
+    private Vector2 velocity;
+
+    [Header("Vida")]
+    [SerializeField] private float maxLife = 100f;
+    [SerializeField] private Image lifeBarFull;
+    private float currentLife;
+
+    [Header("Invulnerabilidad")]
+    [SerializeField] private float fuerzaEmpuje = 10f; 
+    [SerializeField] private float tiempoInvulnerable = 2f; 
+    [SerializeField] private float intervaloParpadeo = 0.2f;
+   
+    private bool esInvulnerable = false;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        currentLife = maxLife;
+        ActualizarBarraDeVida();
     }
 
     void Update()
@@ -21,5 +39,47 @@ public class PlayerMovement : MonoBehaviour
         velocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
         velocity.Normalize();
         rb.velocity = velocity * movSpeed;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Atleta") && !esInvulnerable)
+        {
+            currentLife -= 10f;
+            currentLife = Mathf.Max(currentLife, 0);
+            ActualizarBarraDeVida();
+
+            EmpujarJugador(collision.transform.position);
+            StartCoroutine(Invulnerabilidad());
+        }
+    }
+    private void EmpujarJugador(Vector2 enemigoPosicion)
+    {
+        Vector2 direccionEmpuje = new Vector2(transform.position.x - enemigoPosicion.x, 0).normalized;
+
+        rb.velocity = Vector2.zero;
+        rb.AddForce(direccionEmpuje * fuerzaEmpuje, ForceMode2D.Impulse);
+    }
+
+    private IEnumerator Invulnerabilidad()
+    {
+        esInvulnerable = true;
+        float tiempo = 0f;
+        while (tiempo < tiempoInvulnerable)
+        {
+            spriteRenderer.enabled = !spriteRenderer.enabled;
+            yield return new WaitForSeconds(intervaloParpadeo);
+            tiempo += intervaloParpadeo;
+        }
+
+        spriteRenderer.enabled = true;
+        esInvulnerable = false;
+    }
+    private void ActualizarBarraDeVida()
+    {
+        if (lifeBarFull != null)
+        {
+            lifeBarFull.fillAmount = currentLife / maxLife;
+        }
     }
 }
